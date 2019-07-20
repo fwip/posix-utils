9eeacf37b79bd5cf48983ce1c9f9d93ccb7ddce6
Jemma Nelson <pink.fwip@gmail.com>
2019-07-20 14:51:33 -0700

    Ed: scope test examples to tests

--- pkg/ed/ed_peg_test.go
+++ pkg/ed/ed_peg_test.go
@@ -4,66 +4,6 @@ import (
 	"testing"
 )
 
-// These commands take no addresses and accept no parameters
-var psimpleCmds = []string{
-	"h",
-	"H",
-	"P",
-	"q",
-	"Q",
-	"u",
-}
-
-var pParamCmds = []string{
-	"e",
-	"e file",
-	"E",
-	"E file",
-	"f",
-	"f file",
-}
-
-var pAddrCmds = []string{
-	"=",
-	"10=",
-	"'x=",
-	".=",
-	"$=",
-	"/123/=",
-	`/12\/3/=`,
-	"?123?=",
-	".+1=",
-	"12-=",
-	"$-10=",
-}
-
-var pRangeCmds = []string{
-	"d",
-	".,$d",
-	"3,d",
-	",10d",
-	"2;.+2d",
-	";/hi/d",
-	"/abc/;?123?d",
-}
-
-var pTextCmds = []string{
-	"c\nhi\n.",
-	"10,12c\nhello\nsome\ntext\n.",
-	"c\n. some text .\n.",
-	"3a\nhello\n.",
-	"3i\nhello\n.",
-}
-
-var pMiscCmds = []string{
-	"2kf",
-	"w",
-	"1,4w",
-	"2w newfile",
-	"2a\nhi\n.",
-	",p",
-}
-
 func parse(cmd string) (cmds []Command, err error) {
 
 	out := make(chan Command)
@@ -85,6 +25,15 @@ func parse(cmd string) (cmds []Command, err error) {
 }
 
 func TestSimpleCommandParser(t *testing.T) {
+	// These commands take no addresses and accept no parameters
+	var psimpleCmds = []string{
+		"h",
+		"H",
+		"P",
+		"q",
+		"Q",
+		"u",
+	}
 	for _, s := range psimpleCmds {
 		cmds, err := parse(s + "\n")
 		if err != nil {
@@ -97,6 +46,13 @@ func TestSimpleCommandParser(t *testing.T) {
 }
 
 func TestParamCommandParser(t *testing.T) {
+	var pParamCmds = []string{"e",
+		"e file",
+		"E",
+		"E file",
+		"f",
+		"f file",
+	}
 	for _, s := range pParamCmds {
 		t.Run("paramParse:"+s, func(t *testing.T) {
 			cmds, err := parse(s + "\n")
@@ -112,6 +68,19 @@ func TestParamCommandParser(t *testing.T) {
 }
 
 func TestAddrCommandParser(t *testing.T) {
+	var pAddrCmds = []string{
+		"=",
+		"10=",
+		"'x=",
+		".=",
+		"$=",
+		"/123/=",
+		`/12\/3/=`,
+		"?123?=",
+		".+1=",
+		"12-=",
+		"$-10=",
+	}
 	for _, s := range pAddrCmds {
 		t.Run("addrParse:"+s, func(t *testing.T) {
 			cmds, err := parse(s + "\n")
@@ -127,6 +96,15 @@ func TestAddrCommandParser(t *testing.T) {
 }
 
 func TestRangeCommandParser(t *testing.T) {
+	var pRangeCmds = []string{
+		"d",
+		".,$d",
+		"3,d",
+		",10d",
+		"2;.+2d",
+		";/hi/d",
+		"/abc/;?123?d",
+	}
 	for _, s := range pRangeCmds {
 		t.Run("rangeParse:"+s, func(t *testing.T) {
 			cmds, err := parse(s + "\n")
@@ -142,6 +120,13 @@ func TestRangeCommandParser(t *testing.T) {
 }
 
 func TestTextCommandParser(t *testing.T) {
+	var pTextCmds = []string{
+		"c\nhi\n.",
+		"10,12c\nhello\nsome\ntext\n.",
+		"c\n. some text .\n.",
+		"3a\nhello\n.",
+		"3i\nhello\n.",
+	}
 	for _, s := range pTextCmds {
 		t.Run("textParse:"+s, func(t *testing.T) {
 			cmds, err := parse(s + "\n")
@@ -157,6 +142,14 @@ func TestTextCommandParser(t *testing.T) {
 }
 
 func TestMiscCommandParser(t *testing.T) {
+	var pMiscCmds = []string{
+		"2kf",
+		"w",
+		"1,4w",
+		"2w newfile",
+		"2a\nhi\n.",
+		",p",
+	}
 	for _, s := range pMiscCmds {
 		t.Run("miscParse:"+s, func(t *testing.T) {
 			cmds, err := parse(s + "\n")
